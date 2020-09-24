62b68d1aa8b09707ac809efa8d7ab74f94d2e67d
afmoyar <afmoyar@unal.edu.co>
2020-09-24 02:28:05 -0500

    post request workin

--- GeoSupport_ms/Controllers/CountryController.cs
+++ GeoSupport_ms/Controllers/CountryController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using GeoSupport_ms.Contexts;
 using GeoSupport_ms.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -37,20 +38,50 @@ namespace GeoSupport_ms.Controllers
 
         // POST api/<CountryController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post([FromBody] Country country)
         {
+            try
+            {
+                context.Country.Add(country);
+                context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex) {
+                return BadRequest();
+            }
+            
         }
 
         // PUT api/<CountryController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult Put(int id, [FromBody] Country country)
         {
+            if(country.Id_country == id)
+            {
+                context.Entry(country).State = EntityState.Modified;
+                context.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
         }
 
         // DELETE api/<CountryController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
+            var country = context.Country.FirstOrDefault(p => p.Id_country == id);
+            if (country == null)
+            {
+                context.Country.Remove(country);
+                context.SaveChanges();
+                return Ok();
+            }
+            else {
+                return BadRequest();
+            }
         }
     }
 }
