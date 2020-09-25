18f29e9afb41dcaa34c02b71f183235ac97dcfed
afmoyar <afmoyar@unal.edu.co>
2020-09-24 22:23:39 -0500

    new country controller

--- GeoSupport_ms/Controllers/CountryController.cs
+++ GeoSupport_ms/Controllers/CountryController.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using GeoSupport_ms.Contexts;
-using GeoSupport_ms.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using GeoSupport_ms.Contexts;
+using GeoSupport_ms.Models;
 
 namespace GeoSupport_ms.Controllers
 {
@@ -15,75 +14,97 @@ namespace GeoSupport_ms.Controllers
     [ApiController]
     public class CountryController : ControllerBase
     {
-        private readonly AppDbContext context;
+        private readonly AppDbContext _context;
+
         public CountryController(AppDbContext context)
         {
-            this.context = context;
+            _context = context;
         }
 
-        // GET: api/<CountryController>
+        // GET: api/Countrie
         [HttpGet]
-        public IEnumerable<Country> Get()
+        public async Task<ActionResult<IEnumerable<Country>>> GetCountry()
         {
-            return context.Country.ToList();
+            return await _context.Country.ToListAsync();
         }
 
-        // GET api/<CountryController>/5
+        // GET: api/Countrie/5
         [HttpGet("{id}")]
-        public Country Get(int id)
+        public async Task<ActionResult<Country>> GetCountry(int id)
         {
-            var country = context.Country.FirstOrDefault(p => p.Id_country == id);
+            var country = await _context.Country.FindAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
             return country;
         }
 
-        // POST api/<CountryController>
-        [HttpPost]
-        public ActionResult Post([FromBody] Country country)
+        // PUT: api/Countrie/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountry(int id, Country country)
         {
+            if (id != country.Id_country)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(country).State = EntityState.Modified;
+
             try
             {
-                context.Country.Add(country);
-                context.SaveChanges();
-                return Ok();
+                await _context.SaveChangesAsync();
             }
-            catch (Exception ex) {
-                return BadRequest();
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CountryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
             }
-            
+
+            return NoContent();
         }
 
-        // PUT api/<CountryController>/5
-        [HttpPut("{id}")]
-        public ActionResult Put(int id, [FromBody] Country country)
+        // POST: api/Countrie
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Country>> PostCountry(Country country)
         {
- 
-             if (country.Id_country == id)
-            {
-                context.Entry(country).State = EntityState.Modified;
-                context.SaveChanges();
-                return Ok();
-            }
-            else
-            {
-                return BadRequest();
-            }
-      
+            _context.Country.Add(country);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCountry", new { id = country.Id_country }, country);
         }
 
-        // DELETE api/<CountryController>/5
+        // DELETE: api/Countrie/5
         [HttpDelete("{id}")]
-        public ActionResult Delete(int id)
+        public async Task<ActionResult<Country>> DeleteCountry(int id)
         {
-            var country = context.Country.FirstOrDefault(p => p.Id_country == id);
-            if (country != null)
+            var country = await _context.Country.FindAsync(id);
+            if (country == null)
             {
-                context.Country.Remove(country);
-                context.SaveChanges();
-                return Ok();
+                return NotFound();
             }
-            else {
-                return BadRequest();
-            }
+
+            _context.Country.Remove(country);
+            await _context.SaveChangesAsync();
+
+            return country;
+        }
+
+        private bool CountryExists(int id)
+        {
+            return _context.Country.Any(e => e.Id_country == id);
         }
     }
 }
