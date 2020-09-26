162c610a287cfa141f4caa61161bdd160bec2561
afmoyar <afmoyar@unal.edu.co>
2020-09-25 19:41:46 -0500

    saving

---
+++ GeoSupport_ms/Queries/CountryQuery.cs
@@ -0,0 +1,35 @@
+﻿using GeoSupport_ms.Contexts;
+using GeoSupport_ms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeoSupport_ms.Queries
+{
+    public class CountryQuery : IQuery
+    {
+        private readonly AppDbContext _context;
+
+        public CountryQuery(AppDbContext context)
+        {
+            _context = context;
+        }
+        public List<IModel> getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IModel getByPk()
+        {
+            throw new NotImplementedException();
+        }
+        public List<Country> FindWhereIdContinent(int idContinent)
+        {
+            List<Country> countries = _context.Country
+                .Where(s => s.ContinentId_continent == idContinent).ToList();
+            return countries;
+        }
+
+    }
+}
